27eb003e91161bb504521c4e94a1e1d0a3a8f072
Od1nB <odinbjoernebo@gmail.com>
2025-09-07 11:50:33 +0200

    refactor: main func

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log/slog"
-	"os"
 
 	"github.com/Od1nB/prompter/color"
 	"github.com/Od1nB/prompter/git"
@@ -15,12 +13,12 @@ import (
 var containerEmoji = "🐋"
 
 var (
-	opts            = []path.Option{}
-	maxLen          = flag.Int("max", 40, "set the max amount of chars the first prompt line should be")
-	showContainer   = flag.Bool("showcontainer", false, "display the "+containerEmoji+" emoji at the start of prompt if set")
-	displayHostname = flag.Bool("hostname", false, "display the hostname before the path")
-	showPath        = flag.Bool("showpath", true, "show the 'pwd'")
-	showGit         = flag.Bool("showgit", true, "show the current branch if in a git repo")
+	opts          = []path.Option{}
+	maxLen        = flag.Int("max", 40, "set the max amount of chars the first prompt line should be")
+	showContainer = flag.Bool("showcontainer", false, "display the "+containerEmoji+" emoji at the start of prompt if set")
+	showHostname  = flag.Bool("hostname", false, "display the hostname before the path")
+	showPath      = flag.Bool("showpath", true, "show the 'pwd'")
+	showGit       = flag.Bool("showgit", true, "show the current branch if in a git repo")
 )
 
 func init() {
@@ -31,43 +29,83 @@ func init() {
 }
 
 func main() {
-	var prompt string
-	if *showContainer {
-		prompt += containerEmoji
-	}
+	p := New()
+	fmt.Println(p.out)
+}
 
-	if *displayHostname {
+type prompt struct {
+	out  string
+	def  string
+	errs []error
+}
+
+type printPrompt func() (string, error)
+
+var (
+	containerPrompt printPrompt = func() (string, error) {
+		if !*showContainer {
+			return "", nil
+		}
+		return containerEmoji, nil
+	}
+	hostnamePrompt printPrompt = func() (string, error) {
+		if !*showHostname {
+			return "", nil
+		}
 		hn, err := hostname.New()
 		if err != nil {
-			fmt.Println("⚡")
-			os.Exit(2)
+			return "", fmt.Errorf("hostname err: %w", err)
 		}
-		prompt += hn.String()
+		return hn.String(), nil
 	}
-
-	if *showPath {
-		path, err := path.New(opts...)
+	pathPrompt printPrompt = func() (string, error) {
+		if !*showPath {
+			return "", nil
+		}
+		p, err := path.New(opts...)
 		if err != nil {
-			fmt.Print("⚡")
-			os.Exit(2)
+			return "", fmt.Errorf("path err: %w", err)
 		}
-		if *displayHostname {
-			prompt += color.Paint(color.Cyan, "@")
+
+		if *showHostname {
+			return color.Paint(color.Cyan, "@") + p.String(), nil
 		}
-		prompt += path.String()
+		return p.String(), nil
+	}
+	gitPrompt printPrompt = func() (string, error) {
+		if !*showGit || !git.InRepo() {
+			return "", nil
+		}
+		g, err := git.New()
+		if err != nil {
+			return "", fmt.Errorf("git err: %w", err)
+		}
+		return g.String(), nil
+	}
+)
+
+func New() prompt {
+	p := prompt{
+		def:  "⚡",
+		errs: make([]error, 0),
 	}
 
-	if *showGit {
-		if git.InRepo() {
-			g, err := git.New()
-			if err != nil {
-				slog.Error("err parsing git ", "err", err)
-				os.Exit(2)
-			}
-			prompt += " " + g.String()
+	for _, pr := range []printPrompt{containerPrompt, hostnamePrompt, pathPrompt, gitPrompt} {
+		str, err := pr()
+		if err != nil {
+			p.errs = append(p.errs, err)
 		}
+		p.out += str
 	}
-	prompt += "\n⚡"
 
-	fmt.Print(prompt)
+	switch {
+	case len(p.errs) != 0:
+		p.out = fmt.Sprintf("got errors when creating prompt: %v\n"+p.def, p.errs)
+	case len(p.out) == 0:
+		p.out = p.def
+	default:
+		p.out += "\n" + p.def
+	}
+
+	return p
 }
